e06868a70d706a32db356addb9621143b92a9208
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-05 21:42:29 +0100

    Remove printer management

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.18',
+    'version': '1.0.1.19',
     'category': 'Prepress/Manufacturing',
     'demo': [],
     'depends': ['mrp','prepress_management','mrp_maintenance'],
@@ -18,8 +18,6 @@
         'views/mrp_production_views.xml',
         'views/mrp_bom_views.xml',
         'views/mrp_routing_views.xml',
-        'views/maintenance_views.xml',
-        'views/mrp_workorder_views.xml'
     ],
     'license': 'LGPL-3',
 }
--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,3 @@ from . import mrp_production
 from . import mrp_bom
 from . import stock_move
 from . import mrp_routing
-from . import mrp_workorder
-from . import maintenance
