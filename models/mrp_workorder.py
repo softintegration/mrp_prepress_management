aa7d6bd562986cdf1edac838bec14b28c051b950
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-10-25 12:07:14 +0100

    Manufacturing workorder:Prevent starting of workorder when the parent manufacturing related proof is not «Validated» or «Flashed»

--- models/mrp_workorder.py
+++ models/mrp_workorder.py
@@ -4,6 +4,8 @@ import datetime
 
 from odoo import models, fields, api, _
 from dateutil.relativedelta import relativedelta
+from .mrp_production import AVOIDED_CONFIRM_RELATED_PROOFS_STATES
+from odoo.exceptions import ValidationError
 
 
 class MrpWorkorder(models.Model):
@@ -11,3 +13,14 @@ class MrpWorkorder(models.Model):
 
     planning_id = fields.Many2one('mrp.production.planning')
     partner_id = fields.Many2one(related='product_id.partner_id', string='Customer',store=True,readonly=True)
+
+    def button_start(self):
+        self._check_manufacturing_order_proof()
+        return super(MrpWorkorder,self).button_start()
+
+
+    def _check_manufacturing_order_proof(self):
+        self.ensure_one()
+        if self.production_id.prepress_proof_id and self.production_id.prepress_proof_id.state in AVOIDED_CONFIRM_RELATED_PROOFS_STATES:
+            raise ValidationError(
+                _("Can not start the Workorder %s,the manufacturing order related proof must be Validated or Flashed!") % self.name)
